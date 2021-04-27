213a2b2672300630862a057abf489b0a49164471
Samuel Showalter <samuelrshowalter@gmail.com>
2021-04-26 17:37:22 -0700

    Full project general commit

--- hw3/viterbi.py
+++ hw3/viterbi.py
@@ -24,9 +24,47 @@ def run_viterbi(emission_scores, trans_scores, start_scores, end_scores):
     assert emission_scores.shape[1] == L
     N = emission_scores.shape[0]
 
-    y = []
-    for i in range(N):
-        # stupid sequence
-        y.append(i % L)
-    # score set to 0
-    return (0.0, y)
+    # Init the dp matrices
+    # - dp sequence codes
+    # - dp scores
+    dp_seqs = np.zeros((N, L))
+    dp_scores = -np.inf*np.ones((N, L))
+
+    # Add beginning scores to dp_scores
+    # Seed dynamic program
+    # (start_score + emission)
+    dp_scores[0,:] = start_scores + emission_scores[0,:]
+
+    # Iterate through tokens
+    for i in range(1, N):
+        #Iterature through labels
+        for j in range(L):
+            #Iterate again through labels for preceding one
+            for l in range(L):
+
+                # Sum of previous dp_scores, at a label, emissions scores, trans
+                total_score = dp_scores[i-1][l] + emission_scores[i,j] + trans_scores[l][j]
+
+                #Update if you have found a more probably subseq
+                if (total_score > dp_scores[i,j]):
+                    dp_scores[i,j] = total_score
+                    dp_seqs[i,j] = l
+
+    # Add the ending scores to dp_scores
+    # After the build-up is done
+    dp_scores[N-1, :] += end_scores
+
+    # Get sequence index with highest score from dp_scores
+    # to plug into dp_seqs
+    y = [np.argmax(dp_scores[-1])]
+
+
+    for i in range(1, N):
+        # Dynamically add from dp_seqs using
+        # last added element
+        y.insert(0,dp_seqs[N - i, int(y[0])])
+
+    # Return largest score from dp_scores
+    # (last row, at last index of y)
+    # As well as integers representing best seq
+    return (dp_scores[-1,y[-1]], y)
